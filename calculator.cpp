446d569ece4dd7f5de884dea4450767d2fb38cb1
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    Karmaniuk_commit

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,22 @@
 #include "calculator.h"
 
+//add func
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+//sub func
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
+
+//mul func
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+
+//calculator
+//add sub mul
